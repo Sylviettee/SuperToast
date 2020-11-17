d371e8a50d3d1b2b63843af4e835ca11c5959353
Soviet Kitsune <SovietKitsune@soviet.solutions>
2020-11-16 17:36:26 -0800

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Soviet Kitsune'
 pygments_style = 'sphinx'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
    name = 'SovietKitsune/SuperToast',
-   version = '0.1.0',
+   version = '0.1.1',
    description = 'The better Toast',
    tags = {'toast', 'discordia'},
    license = 'MIT',
