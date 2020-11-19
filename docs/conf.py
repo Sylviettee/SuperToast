a49d57ed059578a6e61fc57b080f273c3525336f
Soviet Kitsune <SovietKitsune@soviet.solutions>
2020-11-18 17:57:47 -0800

    Add spec and main to ignored files

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Soviet Kitsune'
 pygments_style = 'sphinx'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
    name = 'SovietKitsune/SuperToast',
-   version = '0.2.0',
+   version = '0.2.1',
    description = 'The better Toast',
    tags = {'toast', 'discordia'},
    license = 'MIT',
@@ -8,6 +8,6 @@ return {
    homepage = 'https://github.com/SovietKitsune/SuperToast',
    dependencies = {'SinisterRectus/discordia@2.8.4'},
    files = {
-      '**.lua', '!test*', '!util' -- Utils are for running and ops
+      '**.lua', '!spec*', '!util', '!main.lua' -- Utils are for running and ops
    }
 }
