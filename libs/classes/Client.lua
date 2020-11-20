bc36e22000838f3a84dcc651381e964eee3d8465
Soviet Kitsune <SovietKitsune@soviet.solutions>
2020-11-20 13:15:42 -0800

    Inline replies?

--- libs/classes/Client.lua
+++ libs/classes/Client.lua
@@ -1,6 +1,10 @@
 local discordia = require('discordia')
+local pathjoin = require('pathjoin')
+local fs = require('fs')
 
 local class = discordia.class
+
+local splitPath = pathjoin.splitPath
 
 ---@type stringx
 local stringx = require('utils/stringx')
@@ -98,6 +102,126 @@ end
 ---@param command Command
 function Helper:addCommand(command)
    self._commands:push(command)
+end
+
+
+local function parseFile(obj, files)
+   if type(obj) == 'string' then
+      local data, err = fs.readFileSync(obj)
+      if not data then
+         return nil, err
+      end
+      files = files or {}
+      table.insert(files, {table.remove(splitPath(obj)), data})
+   elseif type(obj) == 'table' and type(obj[1]) == 'string' and type(obj[2]) == 'string' then
+      files = files or {}
+      table.insert(files, obj)
+   else
+      return nil, 'Invalid file object: ' .. tostring(obj)
+   end
+   return files
+end
+
+local function parseMention(obj, mentions)
+   if type(obj) == 'table' and obj.mentionString then
+      mentions = mentions or {}
+      table.insert(mentions, obj.mentionString)
+   else
+      return nil, 'Unmentionable object: ' .. tostring(obj)
+   end
+   return mentions
+end
+
+--- Send a reply to a message
+---@param msg Message
+---@param content string|table
+---@param mention boolean
+---@return Message
+function Helper:reply(msg, content, mention)
+   local data, err
+
+   if type(content) == 'table' then
+      local tbl = content
+      content = tbl.content
+
+      if type(tbl.code) == 'string' then
+         content = f('```%s\n%s\n```', tbl.code, content)
+      elseif tbl.code == true then
+         content = f('```\n%s\n```', content)
+      end
+
+      local mentions
+      if tbl.mention then
+         mentions, err = parseMention(tbl.mention)
+         if err then
+            return nil, err
+         end
+      end
+
+      if type(tbl.mentions) == 'table' then
+         for _, mention in ipairs(tbl.mentions) do
+            mentions, err = parseMention(mention, mentions)
+
+            if err then
+               return nil, err
+            end
+         end
+      end
+
+      if mentions then
+         table.insert(mentions, content)
+         content = table.concat(mentions, ' ')
+      end
+
+      local files
+
+      if tbl.file then
+         files, err = parseFile(tbl.file)
+         if err then
+            return nil, err
+         end
+      end
+
+      if type(tbl.files) == 'table' then
+         for _, file in ipairs(tbl.files) do
+            files, err = parseFile(file, files)
+            if err then
+               return nil, err
+            end
+         end
+      end
+
+      data, err = self._api:createMessage(msg.channel.id, {
+         content = content,
+         tts = tbl.tts,
+         nonce = tbl.nonce,
+         embed = tbl.embed,
+         message_reference = {
+            message_id = msg.id,
+            channel_id = msg.channel.id
+         },
+         allowed_mentions = {
+            replied_user = mention or false
+         }
+      }, files)
+   else
+      data, err = self._api:createMessage(msg.channel.id, {
+         content = content,
+         message_reference = {
+            message_id = msg.id,
+            channel_id = msg.channel.id
+         },
+         allowed_mentions = {
+            replied_user = mention or false
+         }
+      })
+   end
+
+   if data then
+      return msg.channel._messages:_insert(data)
+   else
+      return nil, err
+   end
 end
 
 function get:commands()
