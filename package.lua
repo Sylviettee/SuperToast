f6d180fb224ce2d31d5fe49fd8637b91f07722bb
Soviet Kitsune <sovietKitsune@soviet.solutions>
2021-02-16 20:02:59 -0800

    Bump version

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
    name = 'SovietKitsune/SuperToast',
-   version = '0.7.1',
+   version = '0.8.0',
    description = 'The better Toast',
    tags = {'toast', 'discordia'},
    license = 'MIT',
